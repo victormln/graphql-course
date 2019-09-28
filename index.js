a8619004ed44b31a8417167c56e9c7d40e2a680e
victormln <victor.molinaf@gmail.com>
2019-09-28 13:28:57 +0200

    Adding graphiql to access on http://localhost:3000/api

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict'
 
 const { graphql, buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
+
+const app = express()
+const port = process.env.port || 3000
 
 // Definimos el schema inicial
 const schema = buildSchema(`
@@ -20,7 +25,12 @@ const resolvers = {
     }
 }
 
-// Ejecutamos graphql
-graphql(schema, '{ saludo }', resolvers).then((data) => {
-    console.log(data)
+app.use('/api', gqlMiddleware({
+    schema: schema,
+    rootValue: resolvers,
+    graphiql: true
+}))
+
+app.listen(port, () => {
+    console.log(`Server is listening at: http://localhost:${port}/api`)
 })
